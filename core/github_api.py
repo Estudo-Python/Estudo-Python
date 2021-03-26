c841aa52ce804a7682d204c98d4b97aa12bbcbd3
felipe.nogueira <felipematheus.ns0@gmail.com>
2021-03-26 13:44:09 -0300

    Inserido test com pytest e integrado com o Travis
    close #18

--- core/github_api.py
+++ core/github_api.py
@@ -2,8 +2,8 @@ import requests
 
 
 def buscar_avatar(usuario):
-    """
-    Busca o avatar de um usuário no GitHub
+    """Busca o avatar de um usuário no GitHub
+
     :param usuario: str com nome de usuário no github
     :return: str com link do avatar
     """
