db70904bce9ee87508682ef6b999e6c675dd0b1a
Nicole <vieiranicolealice@gmail.com>
2021-03-27 15:52:50 -0300

    Isolado os testes através da implementação do método rollback.

--- core/tests/test_spam/test_usuarios.py
+++ core/tests/test_spam/test_usuarios.py
@@ -3,23 +3,31 @@ from core.spam.modelos import Usuario
 
 
 def test_salvar_usuario():
+    # SetUp
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
+    ##
     usuario = Usuario(nome='Felipe')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
+    # Tear Down
     sessao.roll_back()
     sessao.fechar()
     conexao.fechar()
+    ##
 
 
 def test_listar_usuarios():
+    # SetUp
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
+    ##
     usuarios = [Usuario(nome='Felipe'), Usuario(nome='Matheus')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
+    # Tear Down
     sessao.roll_back()
     sessao.fechar()
     conexao.fechar()
+    ##
