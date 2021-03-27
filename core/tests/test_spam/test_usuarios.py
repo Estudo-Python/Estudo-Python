4e094bc44735661a3f5e3ca68ab2080bea13c9f5
Nicole <vieiranicolealice@gmail.com>
2021-03-27 17:59:05 -0300

    Criado teste_envio_para_base_de_usuarios.py para testa a quantidade de emails enviados.
    Para o teste dar certo, tive que alterar um código de produção, na classe EnviadorDeSpam

--- core/tests/test_spam/test_usuarios.py
+++ core/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from core.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Felipe')
+    usuario = Usuario(nome='Felipe', email='felipematheus.ns0@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Felipe'), Usuario(nome='Matheus')]
+    usuarios = [
+        Usuario(nome='Felipe', email='felipematheus.ns0@gmail.com'),
+        Usuario(nome='Matheus', email='fe_matheus@hotmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
