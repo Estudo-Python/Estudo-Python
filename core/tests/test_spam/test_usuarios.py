6a6ed7ae63d4f6120c03a8d20c22898988a129d9
Nicole <vieiranicolealice@gmail.com>
2021-03-27 16:12:57 -0300

    Criados os fixtures de conexao e sessao, onde temos o setup e teardown.

--- core/tests/test_spam/test_usuarios.py
+++ core/tests/test_spam/test_usuarios.py
@@ -1,33 +1,37 @@
+import pytest
+
 from core.spam.db import Conexao
 from core.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
+@pytest.fixture
+def conexao():
     # SetUp
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
-    ##
+    conexao_obj = Conexao()
+    yield conexao_obj
+    # Tear Down
+    conexao_obj.fechar()
+
+
+@pytest.fixture
+def sessao(conexao):
+    # SetUp
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    # Tear Down
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Felipe')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    # Tear Down
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
-    ##
 
 
-def test_listar_usuarios():
-    # SetUp
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
-    ##
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Felipe'), Usuario(nome='Matheus')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    # Tear Down
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
-    ##
+
