9190fb817bd17363e758d627a8cd3dc58c6fb7c8
Nicole <vieiranicolealice@gmail.com>
2021-03-28 20:37:03 -0300

    Finalizada integração de testes.
    close #28

--- core/tests/test_spam/test_github_api.py
+++ core/tests/test_spam/test_github_api.py
@@ -1,12 +1,27 @@
-from unittest.mock import Mock
+import pytest
 
+from unittest.mock import Mock
 from core import github_api
 
 
-def teste_buscar_avatar():
+@pytest.fixture()
+def avatar_url(mocker):
     resp_mock = Mock()
-    github_api.requests.get = Mock(return_value=resp_mock)  # Isolando .get de requests
-    resp_mock.json.return_value = {'avatar_url': 'https://avatars.githubusercontent.com/u/62901711?v=4'}
 
+    get_mock = mocker.patch('core.github_api.requests.get')
+    get_mock.return_value = resp_mock
+
+    url = 'https://avatars.githubusercontent.com/u/62901711?v=4'
+    resp_mock.json.return_value = {'avatar_url': url}
+
+    return url
+
+
+def teste_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('felipeno')
     assert 'https://avatars.githubusercontent.com/u/62901711?v=4' == url
+
+
+def teste_integracao_buscar_avatar():
+    url = github_api.buscar_avatar('felipeno-ambev')
+    assert 'https://avatars.githubusercontent.com/u/79462474?v=4' == url
