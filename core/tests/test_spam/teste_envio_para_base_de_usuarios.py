0a5260de69e1009c1256dd48c818c7b51e3c2c8c
Nicole <vieiranicolealice@gmail.com>
2021-03-28 14:55:25 -0300

    Criada injeção de dependências com MOCK

--- core/tests/test_spam/teste_envio_para_base_de_usuarios.py
+++ core/tests/test_spam/teste_envio_para_base_de_usuarios.py
@@ -20,7 +20,7 @@ from core.spam.modelos import Usuario
 def test_quantidade_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'felipe.nsilva@gmail.com',
@@ -28,3 +28,32 @@ def test_quantidade_de_spam(sessao, usuarios):
         'Estudando Pytools'
     )
     assert len(usuarios) == enviador.quantidade_de_emails_enviados
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        self.parametros_de_envio = None
+        self.quantidade_de_emails_enviados = 0
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.quantidade_de_emails_enviados += 1
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Matheus', email='fe_matheus@hotmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'felipe.nsilva@gmail.com',
+        'Estudo python',
+        'Estudando Pytools'
+    )
+    assert enviador.parametros_de_envio == (
+        'felipe.nsilva@gmail.com',
+        'fe_matheus@hotmail.com',
+        'Estudo python',
+        'Estudando Pytools'
+    )
